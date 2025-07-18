df4b39fad7610ab16f7e1ad12567e6b314466177
Gustavo Garcia <gustavogarciaci1@gmail.com>
2025-07-18 14:44:23 -0300

    feat: user pydantic schema

---
+++ src/adapters/api/schemas/user.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+
+
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50, unique=True)
+    email: EmailStr = Field(..., unique=True)
+    password: str = Field(..., min_length=8)
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = Field(None, min_length=3, max_length=50, unique=True)
+    email: Optional[EmailStr] = Field(None, unique=True)
+
+class UserResponse(BaseModel):
+    id: UUID
+    username: str
+    email: EmailStr
+    created_at: datetime
+    updated_at: Optional[datetime]
+
+    class Config:
+        from_attributes = True

45b326902c5f144c6e83edeaa897c57b36981d8f
Gustavo Garcia <gustavogarciaci1@gmail.com>
2025-07-19 17:59:05 -0300

    chore: refactor get user routes to protected routes

--- tests/integration/test_get_user_route.py
+++ tests/integration/test_get_user_route.py
@@ -6,14 +6,24 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_get_user_success(async_session, client, make_user_api):
+async def test_get_user_success(
+    async_session,
+    client,
+    make_user_api,
+    make_token_api,
+):
     user = await make_user_api(
         username='testuser',
         email='testuser@example.com',
         password_hash='testpassword',
     )
 
-    response = client.get(f'/api/v1/users/{user.id}')
+    token = make_token_api('testuser@example.com', 'testpassword')
+
+    response = client.get(
+        f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json()['username'] == 'testuser'
@@ -29,9 +39,22 @@ async def test_get_user_not_found(
     async_session,
     client,
     make_user_api,
+    make_token_api,
 ):
+    await make_user_api(
+        username='testuser',
+        email='testuser@example.com',
+        password_hash='testpassword',
+    )
+
     random_id = uuid4()
-    response = client.get(f'/api/v1/users/{random_id}')
+
+    token = make_token_api('testuser@example.com', 'testpassword')
+
+    response = client.get(
+        f'/api/v1/users/{random_id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()['detail'] == f'User with id {random_id} not found'
@@ -42,8 +65,17 @@ async def test_get_user_internal_server_error(
     async_session,
     client,
     make_user_api,
+    make_token_api,
 ):
+    await make_user_api(
+        username='testuser',
+        email='testuser@example.com',
+        password_hash='testpassword',
+    )
+
     random_id = uuid4()
+
+    token = make_token_api('testuser@example.com', 'testpassword')
 
     with patch(
         'src.adapters.api.routers.get_user.get_user_factory',
@@ -53,7 +85,10 @@ async def test_get_user_internal_server_error(
             'Database connection failed',
         )
 
-        response = client.get(f'/api/v1/users/{random_id}')
+        response = client.get(
+            f'/api/v1/users/{random_id}',
+            headers={'Authorization': f'Bearer {token}'},
+        )
 
         assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
         assert response.json()['detail'] == 'Database connection failed'
